c5a152b8e8fc18710b6ccf3f86df4daf163dacb4
RXAliman <rovicxavier150@gmail.com>
2026-01-07 11:25:21 +0800

    Reset codebase for v2

--- lib/services/constants.dart
+++
@@ -1,8 +0,0 @@
-// Hive Boxes
-const bloodPressureLogBoxName = "blood pressure log";
-const bloodSugarLogBoxName = "blood sugar log";
-const exerciseLogBoxName = "exercise log";
-const mealLogBoxName = "meal log";
-const sleepLogBoxName = "sleep log";
-const waterIntakeLogBoxName = "water intake log";
-const weightLogBoxName = "weight log";
